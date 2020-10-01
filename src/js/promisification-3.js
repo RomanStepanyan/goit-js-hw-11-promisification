5f3f8a05e11df535697fc1a06403e1376281e925
RomanStepanyan <stepik10@gmail.com>
2020-10-02 02:29:52 +0300

    Some correction to Promisification-3 are added

--- src/js/promisification-3.js
+++ src/js/promisification-3.js
@@ -1,32 +1,22 @@
-'use strict'
-
-const { resolve } = require("core-js/fn/promise");
-
 // Перепиши функцию makeTransaction() так, чтобы она не использовала callback-функции onSuccess 
 // и onError, а принимала всего один параметр transaction и возвращала промис.
-
+'use strict'
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
 const makeTransaction = transaction => {
-    return new Promise(resolve, reject => {
+    return new Promise ((resolve, reject) => {
         const delay = randomIntegerFromInterval(200, 500);
         setTimeout(() => {
             const canProcess = Math.random() > 0.3;
-
             if (canProcess) {
-            resolve(transaction.id, delay);
+                resolve(transaction.id, delay);
             } else {
-            reject(transaction.id);
+                reject(transaction.id);
             }
         }, delay);
-
-
     })
-  
-
-  
 };
 
 const logSuccess = (id, time) => {
@@ -37,16 +27,6 @@ const logError = id => {
   console.warn(`Error processing transaction ${id}. Please try again later.`);
 };
 
-/*
- * Работает так
- */
-// makeTransaction({ id: 70, amount: 150 }, logSuccess, logError);
-// makeTransaction({ id: 71, amount: 230 }, logSuccess, logError);
-// makeTransaction({ id: 72, amount: 75 }, logSuccess, logError);
-// makeTransaction({ id: 73, amount: 100 }, logSuccess, logError);
-/*
- * Должно работать так
- */
 makeTransaction({ id: 70, amount: 150 })
   .then(logSuccess)
   .catch(logError);
